b8c2e3d7ff8071661935af82f07ce2a6922b2a94
Mathk <leemumc@gmail.com>
2025-01-21 17:42:40 -0800

    Reapply "Nuking from orbit"
    This reverts commit b33b9802dae14ddd90325662f2485ca514665558.

---
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase  {
+  public enum ElevatorTarget {}
+
+  public Elevator(ElevatorIO io) {}
+
+  @Override
+  public void periodic(){
+
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  public enum Target {
+    
+  }
+
+  public Intake(IntakeIO io) {}
+
+
+  @Override
+  public void periodic(){
+
+  }
+
+}
