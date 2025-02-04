281a51f81fd1b21406cc143a242f8d23d96848ad
Noland <nolan.bjorn.schuler@gmail.com>
2025-02-04 15:10:49 -0800

    Changed the Names of the Target States, added more stuff in description
    changed the names of the target states so they sound a lot more appealing. Also I'm trying to make the pivot move to a certain angle depending on the mode that it is in (AKA methods that act on what the angle is). I also added a little getter.

--- src/main/java/frc/robot/subsystems/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -1,35 +1,45 @@
 package frc.robot.subsystems.pivot;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.pivot.PivotIO.PivotIOInputs;
 
 
 public class Pivot extends SubsystemBase {
   private PivotTarget targetState;
+  private double pivotPosition;
+  private PivotIO pivotIO;
+
+
   public enum PivotTarget{
-    SCORE_L4,
-    SCORE_L3,
-    SCORE_L2,
-    SCORE_L1,
+    L4,
+    L3,
+    L2,
+    L1,
     INTAKE,
     STOW,
     ZEROING;
   }
 
-  
+  //getter
+  public double getPosition() {
+    return pivotPosition;
+  }
+
   public Pivot(PivotIO io) {
     //if the Pivot is at a certain mode, it will go to a certain angle
-      switch(targetState)  {
-        case SCORE_L1: 
-        case SCORE_L2: 
-        case SCORE_L3:
-        case SCORE_L4: 
+      switch(targetState)  { //FIXME: ADD GETTING THE POSITION TARGETS
+        case L1: 
+        case L2: 
+        case L3:
+        case L4: 
+        case INTAKE:
+        case STOW:
+        case ZEROING:
     }
   }
 
 
   @Override
-  public void periodic(){
-  
-  }
+  public void periodic(){}
 
 }
